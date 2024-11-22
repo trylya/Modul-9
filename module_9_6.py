a5648b7df82427dc8586f83c6633a394c8b8444c
Лена <trylya@mail.ru>
2024-11-23 00:22:05 +0300

    Генераторы

---
+++ module_9_6.py
@@ -0,0 +1,10 @@
+def all_variants(text):
+    x = 0
+    y = 0
+    for x in range(len(text)):
+        for y in range(x + 1, len(text) + 1):
+            yield text[x:y]
+
+a = all_variants("abc")
+for i in a:
+    print(i)
